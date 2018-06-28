cd38fb2390a3068d171f01e51bd2993a11135b98
BULBUL AHMED <bulbulahmed.cse@yahoo.com>
2018-06-28 16:27:16 +0600

    Hometask Day02

--- AndroidGovtProject/src/day02/HomeTask1.java
+++ AndroidGovtProject/src/day02/HomeTask1.java
@@ -1,10 +1,34 @@
-
 package day02;
 
 import java.util.Scanner;
 
 public class HomeTask1 {
+
     public static void main(String[] args) {
-      
+        Scanner sc = new Scanner(System.in);
+
+        int i, j, num1;
+        float num2;
+
+        System.out.println("Enter the value of Number1 and Number 2");
+        num1=sc.nextInt();
+        num2=sc.nextFloat();
+        
+        System.out.println("Enter the value of i & j ");
+        i=sc.nextInt();
+        j=sc.nextInt();
+        
+        if(i==1 && j==2){
+            System.out.println("Addition is "+(num1+num2));
+        }else if(i==0 && j<5){
+            System.out.println("Substract is "+(num1-num2));
+        }else if(i>20 || j<=20){
+            System.out.println("Multiplication is "+(num1*num2));
+        }else if(i==30 || j==15){
+            System.out.println("Division is "+(num1/num2));
+        }else{
+            System.out.println("Input is wrong");
+        }
+
     }
 }
--- AndroidGovtProject/src/day02/HomeTask2.java
+++ AndroidGovtProject/src/day02/HomeTask2.java
@@ -1,7 +1,21 @@
 
 package day02;
 
+import java.util.Scanner;
+
 
 public class HomeTask2 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter any positive   number");
+        int a = sc.nextInt();
+        
+        
+        if(a%2==0){
+            System.out.println("Number is even");
+        }else{
+            System.out.println("Number is odd");
+        }
+    }
 }
